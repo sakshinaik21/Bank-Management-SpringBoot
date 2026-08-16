6f14173a61803cd0528f4b91233b628e7e5787ff
sakshinaik21 <sakshinaik0021@gmail.com>
2026-08-17 00:38:42 +0530

    Bank Management Spring Boot project

---
+++ src/main/java/jsp_springBoot/SpringBootBankManagementApplication.java
@@ -0,0 +1,13 @@
+package jsp_springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBankManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jsp_springBoot/SpringBootBankManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
